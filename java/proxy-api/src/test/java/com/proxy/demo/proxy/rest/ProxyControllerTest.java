a95439eb98cb96c6caec6c4ed9a6568448d0d0e8
Pawel Malczyk <malczyk.pawel@gmail.com>
2026-03-12 10:11:03 +0100

    Caching

--- java/proxy-api/src/test/java/com/proxy/demo/proxy/rest/ProxyControllerTest.java
+++ java/proxy-api/src/test/java/com/proxy/demo/proxy/rest/ProxyControllerTest.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
@@ -22,9 +27,19 @@ import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 
 
-//TODO: test of timeouts
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProxyControllerTest {
+
+  @Container
+  static GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine")
+      .withExposedPorts(6379);
+
+  @DynamicPropertySource
+  static void redisProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.redis.host", redis::getHost);
+    registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+  }
 
   @Autowired
   private WebApplicationContext webApplicationContext;
