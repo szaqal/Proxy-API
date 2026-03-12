0b7fa539b23a30ae0e2093cc63be5b519f13dd29
Pawel Malczyk <malczyk.pawel@gmail.com>
2026-03-12 07:30:33 +0100

    Work

--- java/proxy-api/src/main/java/com/proxy/demo/proxy/services/impl/ProxyServiceImpl.java
+++ java/proxy-api/src/main/java/com/proxy/demo/proxy/services/impl/ProxyServiceImpl.java
@@ -12,26 +12,25 @@ import java.util.Map;
 @Service
 public class ProxyServiceImpl implements ProxyService {
 
-  @Value("${OPEN_METEO_API_FORECAST_URL:https://api.open-meteo.com/v1/forecast}")
-  private String apiUrl;
-
   private final RestClient restClient;
 
-  public ProxyServiceImpl(RestClient.Builder restClientBuilder) {
-    this.restClient = restClientBuilder.build();
+  public ProxyServiceImpl() {
+    this.restClient = RestClient.builder()
+        .baseUrl("https://api.open-meteo.com/")
+        .build();
   }
 
   @Override
   public void loadWeatherData(Map<String, String> params) {
     String response = restClient.get()
         .uri(uriBuilder -> {
-          uriBuilder.path(apiUrl);
+          uriBuilder.path("/v1/forecast");
           params.forEach(uriBuilder::queryParam);
           return uriBuilder.build();
         })
         .retrieve()
         .body(String.class);
 
-    log.info("Loaded {} {} {}", apiUrl, params, response);
+    log.info("Loaded {} {}", params, response);
   }
 }
