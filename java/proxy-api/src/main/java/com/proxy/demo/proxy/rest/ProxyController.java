9c6bf87e9fe9c81a3f938cd33199970cad52f5f5
Pawel Malczyk <malczyk.pawel@gmail.com>
2026-03-12 06:56:39 +0100

    Work

--- java/proxy-api/src/main/java/com/proxy/demo/proxy/rest/ProxyController.java
+++ java/proxy-api/src/main/java/com/proxy/demo/proxy/rest/ProxyController.java
@@ -3,6 +3,7 @@ package com.proxy.demo.proxy.rest;
 import com.proxy.demo.proxy.services.api.ProxyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
